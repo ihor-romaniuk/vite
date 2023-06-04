addc119143da35d1362cf888952b70a158c7f472
ihor-romaniuk <ihor.romaniuk@raccoongang.com>
2023-06-04 17:55:01 +0300

    configuration options

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,45 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  base: '/vite',
-});
+export default defineConfig({});
+
+// export default defineConfig(({ command, mode, ssrBuild }) => {
+//   console.log(command, mode, ssrBuild); // serve development false
+//
+//   if (mode === 'production') {
+//     return { base: '/vite', }
+//   }
+//
+//   return {};
+// });
+
+// export default defineConfig(async ({ command, mode}) => {
+//   const data = await someAsycFynction();
+//   return {...data};
+// });
+
+// export default defineConfig(({ command, mode }) => {
+//   const env = loadEnv(mode, process.cwd(), ''); // return all vars
+//   const envVite = loadEnv(mode, process.cwd(), 'VITE_'); // returns vars with VITE_
+//   return {};
+// });
+
+// export default defineConfig({
+//   // base: '/vite',
+//   clearScreen: false,
+//   envPrefix: 'APP_',
+//   logLevel: 'info',
+//   envDir: 'envs',
+//   server: {
+//     open: 'index.html',
+//     port: 3000,
+//     // strictPort: true,
+//   },
+//   preview: {
+//     open: 'index.html',
+//     port: 8080,
+//     strictPort: true,
+//   },
+//   css: {
+//     devSourcemap: true,
+//   }
+// });
